9c6b4bf23ae9a5c6b4915139d0bdc5088d226f9c
Martyna Swiatek <martynaswiatek24@gmail.com>
2023-03-30 10:27:31 +0200

    js transform

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 {
-    const onChangeBackgroundCLick = () => {
-
-        const button = document.querySelector(".js-button");
+    const onChangeBackgroundClick = () => {
         const body = document.querySelector(".js-body");
         const themeName = document.querySelector(".js-themeName");
 
-        button.addEventListener("click", onChangeBackgroundCLick);
         body.classList.toggle("dark");
 
         themeName.innerText = body.classList.contains("dark")
@@ -14,7 +11,9 @@
     };
 
     const init = () => {
-        onChangeBackgroundCLick();
+        const button = document.querySelector(".js-button");
+
+        button.addEventListener("click", onChangeBackgroundClick);
     };
 
     init();
